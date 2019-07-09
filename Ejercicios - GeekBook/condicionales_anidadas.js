2190a5e6e3eb2c30a8e279d210a82fb308a66ec2
disaisoft <disaisoft@gmail.com>
2019-07-08 21:39:58 -0500

    se agregaron las condicionales anidadas

---
+++ Ejercicios - GeekBook/condicionales_anidadas.js
@@ -0,0 +1,12 @@
+var num = 11;
+
+if (num < 10) {
+   console.log("El numero es menor a 10");
+} else {
+  if (num > 10) {
+     console.log("El numero es mayor a 10");
+ } else {
+  console.log("El numero es igual a 10");
+ }
+
+}
---
+++ Ejercicios - GeekBook/condicionales_anidadas2.js
@@ -0,0 +1,10 @@
+var num = 10;
+
+if (num < 10) {
+   console.log("El numero es menor a 10");
+} else if (num > 10) {
+  console.log("El numero es mayor a 10");
+} else {
+  console.log("El numero es igual a 10");
+}
+
