0d284e12540afc47ef127ad478576a72f4138270
disaisoft <disaisoft@gmail.com>
2019-07-08 21:14:56 -0500

    nuevos archivos

---
+++ Ejercicios - GeekBook/number.js
@@ -0,0 +1,9 @@
+var num = 12;
+
+if (num < 10) {
+   console.log("El numero es menor a 10");
+}
+
+if (num >= 10) {
+   console.log("El numero es igual o mayor a 10");
+}
---
+++ Ejercicios - GeekBook/number2.js
@@ -0,0 +1,10 @@
+var num = 15;
+
+if (num < 10) {
+   console.log("El numero es menor a 10");
+}
+
+else {
+   console.log("El numero es igual o mayor a 10");
+}
+
